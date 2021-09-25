27fa1a8201968a776f15b913c74713c22a5b74d4
Arkan Laki <arkan.leki@gmail.com>
2021-09-25 16:47:44 +0300

    pending

--- src/components/Header.js
+++ src/components/Header.js
@@ -44,6 +44,9 @@ const Header = ({ addpay, addTrade, group , searchTrader, addOrder, groups, setG
                             <li className="nav-item">
                                 <Link className="nav-link" to="/payments" >پارەوەرگرتن</Link>
                             </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/pending" >دەرچوون لە کۆگا</Link>
+                            </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     لقەکان
